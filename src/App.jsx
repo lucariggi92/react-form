5ce4d6d5cfa91fdb5785dfec360c45865b64686c
lucariggi92 <riggiluca92@gmail.com>
2025-12-01 16:50:02 +0100

    feat: Appjsx structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* HEADER */}
+      <header>
+        <div className='container py-2'>Lista Articoli</div>
+      </header>
+      {/* 
+      {/* //MAIN */}
+
+
+      {/* FORM INPUT */}
+      <main className='container'>
+        <div className='py-5 d-flex gap-3 mb-3"'>
+          <form action="" onInput={Text} className='border'>aggiungi qui</form>
+          <label for="exampleInputEmail1" class="form-label">Lista</label>
+
+
+          {/* BUTTON */}
+          <button className='btn btn-primary warning p-x-5'>Aggiungi</button>
+        </div>
+
+        {/* LIST */}
+        <ul>
+          <li>lista vuota</li>
+           <li>lista vuota</li>
+            <li>lista vuota</li>
+             <li>lista vuota</li>
+        </ul>
+
+      </main>
+
+
+
     </>
   )
 }
